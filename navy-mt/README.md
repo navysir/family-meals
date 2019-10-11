0029d75ec3584220b962ce3de236fa82b866ab9c
navy <>
2019-10-11 18:23:04 +0800

    美团网重构项目搭建

---
+++ navy-mt/README.md
@@ -0,0 +1,22 @@
+# navy-mt
+
+> 海军重构美团网，提升自己技术
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ navy-mt/components/public/header/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="n-header">
+    <el-row :gutter="15">
+    <el-col :span="8"><geo/></el-col>
+    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    <el-col :span="8"><div class="grid-content bg-purple"></div></el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+import geo from './geo.vue'
+export default {
+    components:{
+        geo
+    },
+};
+</script>>
+<style scoped lang="scss">
+@import "@/assets/css/public/layout.scss"
+</style>
